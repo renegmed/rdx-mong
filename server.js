e1c57e26051ae843afe948814cd4ada2f9bb4e21
rene <renegmed@yahoo.com>
2018-12-06 08:38:45 -0500

    automated server restart after code update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello!!!'));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // To run this 
-// $ node server
+// $ npm run server
